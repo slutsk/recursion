c7278da5094904bff25dd783d11daa06c683b63f
slutsk <54749775+slutsk@users.noreply.github.com>
2020-02-15 22:30:23 +0300

    Add files via upload

---
+++ slutsk_recursion_19_0.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+char arr[26];
+int n;
+void print_array(){
+    for(int i = 0; i < n; i++)
+        cout << arr[i] << ' ';
+    cout << "\n";
+}
+void rec(int index){
+    if(index == n){
+        print_array();
+        return;
+    }
+    for(char i = 'a'; i < 'a' + n; i++){
+        arr[index] = i;
+        rec(index+1);
+    }
+
+}
+
+int main(){
+    freopen("out.txt", "w", stdout);
+    cin >> n;
+    rec(0);
+    return 0;
+}
+
