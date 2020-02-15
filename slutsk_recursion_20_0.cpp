f02346f5fc0ec7ab45b12fed248d5300f511d5be
slutsk <54749775+slutsk@users.noreply.github.com>
2020-02-15 23:15:44 +0300

    Add files via upload

---
+++ slutsk_recursion_20_0.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+int arr[10];
+bool yOn[10]={false};
+int n;
+void print_array(){
+    for(int i = 0; i < n; i++)
+        cout << arr[i] << ' ';
+    cout << "\n";
+}
+void rec(int index){
+    if(index == n){
+        print_array();
+        return;
+    }
+    for(int i = 0; i < n; i++){
+        if(yOn[i] == false){
+        yOn[i] = true;
+        arr[index] = i+1;
+        rec(index+1);
+        yOn[i] = false;
+        }
+    }
+
+}
+
+int main(){
+    cin >> n;
+    rec(0);
+    return 0;
+}
+
