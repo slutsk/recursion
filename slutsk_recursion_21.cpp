f453f01d6eb506db18b20dd56905c89d262d5aeb
slutsk <54749775+slutsk@users.noreply.github.com>
2020-02-16 08:17:43 +0300

    Add files via upload

--- slutsk_recursion_21.cpp
+++ slutsk_recursion_21.cpp
@@ -1,5 +1,13 @@
 #include<iostream>
 using namespace std;
+bool is_true(string s);
+int main(){
+    string s;
+    cin >> s;
+    cout << (is_true(s) ? "Yes":"No");
+    return 0;
+}
+/*
 bool is_true(string s){
    int open=0, close=0;
    for(int i=0; i< s.length(); i++){
@@ -8,10 +16,15 @@ bool is_true(string s){
        if(close>open) return false;
    }
    return open==close;
+}*/
+
+bool is_true(string s){
+   int  b = 0; // balance
+   for(int i=0; i< s.length(); i++){
+       if(s[i]=='(') b++;
+       else b--;
+       if(b < 0) return false;
+   }
+   return b == 0;
 }
-int main(){
-    string s;
-    cin >> s;
-    cout << (is_true(s) ? "Yes":"No");
-    return 0;
-}
+
