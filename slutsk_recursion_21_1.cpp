e471180c3628528a0e12c8aab2aa42cdb1e38ff8
slutsk <54749775+slutsk@users.noreply.github.com>
2020-02-16 09:57:08 +0300

    Add files via upload

---
+++ slutsk_recursion_21_1.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+int n;
+//
+string brackets(20, ' ');
+bool is_true(string str){
+   int  b = 0;
+   for(int i=0; i< 2*n; i++){
+       if(str[i]=='(') b++;
+       else b--;
+       if(b < 0) return false;
+   }
+   return b == 0;
+}
+
+void print_brackets(){
+    for(int i = 0; i < 2*n; i++) cout << brackets[i] << " ";
+    cout << endl;
+}
+void brackets_generator(int index){
+    if(index==2*n){
+        if(is_true(brackets)) print_brackets();
+        return;
+    }
+    //Убрали for
+        brackets[index] = '(';
+        brackets_generator(index+1);
+        brackets[index] = ')';
+        brackets_generator(index+1);
+
+}
+
+int main(){
+    cin >> n;
+    brackets_generator(0);
+    return 0;
+}
+
